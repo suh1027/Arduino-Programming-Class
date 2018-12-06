dd81e0b082c5e7e68d0f6365dff6a6fc6d262023
SeoJiHoon <suh1027@naver.com>
2018-12-06 17:47:03 +0900

    servo

---
+++ servo2/servo2.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+
+Servo myServo;
+
+void setup() {
+  // put your setup code here, to run once:
+  myServo.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val = analogRead(A0);
+  int rad = map(val,0,1023,0,120);
+  myServo.write(rad);
+  delay(15);
+}
